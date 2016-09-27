7a6cabc5c5f5afce2296d978ede5f74510550cc3
AhmedShab <ahmed.vuw@gmail.com>
2016-09-27 13:36:05 +1300

    added image and fixed the style in the home page

--- ATMMachine/App_Start/BundleConfig.cs
+++ ATMMachine/App_Start/BundleConfig.cs
@@ -26,6 +26,7 @@ namespace ATMMachine
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
