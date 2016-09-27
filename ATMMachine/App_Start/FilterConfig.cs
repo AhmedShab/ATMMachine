80f1ff577e3748449e42b01b37b3ed259577c65f
AhmedShab <ahmed.vuw@gmail.com>
2016-09-27 22:18:44 +1300

    Create an account is working at this stage

--- ATMMachine/App_Start/FilterConfig.cs
+++ ATMMachine/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace ATMMachine
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
             filters.Add(new MyLoggingFilterAttribute());
         }
     }
